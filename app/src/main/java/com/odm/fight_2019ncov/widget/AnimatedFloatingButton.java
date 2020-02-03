f2c2fa3c23fbe46582dfc5b847d363629da88c12
DMingOu <dmingou0529@gmail.com>
2020-02-04 00:00:50 +0800

    type:update content:增加了悬浮按钮的平移动画

--- app/src/main/java/com/odm/fight_2019ncov/widget/AnimatedFloatingButton.java
+++ app/src/main/java/com/odm/fight_2019ncov/widget/AnimatedFloatingButton.java
@@ -3,6 +3,8 @@ package com.odm.fight_2019ncov.widget;
 import android.animation.Animator;
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewPropertyAnimator;
 import android.view.animation.LinearInterpolator;
@@ -12,6 +14,7 @@ import androidx.annotation.Nullable;
 
 import com.blankj.utilcode.util.LogUtils;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.odm.fight_2019ncov.App;
 
 /**
  * @description: 带动画的悬浮按钮
@@ -23,11 +26,12 @@ public class AnimatedFloatingButton extends FloatingActionButton {
     ViewPropertyAnimator animator ;
     boolean isAnimating = false;
 
-    Animator.AnimatorListener  animatorListener;
+    Animator.AnimatorListener  hideAnimatorListener;
+    Animator.AnimatorListener  showAnimatorListener;
 
     public AnimatedFloatingButton(@NonNull Context context) {
         super(context);
-        LogUtils.d("第一层构造");
+
         initAnimation();
     }
 
@@ -38,7 +42,6 @@ public class AnimatedFloatingButton extends FloatingActionButton {
      */
     public AnimatedFloatingButton(@NonNull Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
-        LogUtils.d("第二层构造");
         initAnimation();
     }
 
@@ -50,7 +53,6 @@ public class AnimatedFloatingButton extends FloatingActionButton {
      */
     public AnimatedFloatingButton(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        LogUtils.d("第三层构造");
         initAnimation();
     }
 
@@ -58,23 +60,43 @@ public class AnimatedFloatingButton extends FloatingActionButton {
         //初始化 ViewPropertyAnimator
         if (animator == null) {
             animator = this.animate();
-            animator = this.animate();
             animator.setDuration(300);
             animator.setInterpolator(new LinearInterpolator());
         }
 
-        if(animatorListener == null) {
-            //初始化 动画监听事件
-            animatorListener = new Animator.AnimatorListener() {
+        if(hideAnimatorListener == null) {
+            //初始化 隐藏动画监听事件
+            hideAnimatorListener = new Animator.AnimatorListener() {
                 @Override
                 public void onAnimationStart(Animator animation) {
                     isAnimating = true;
+                }
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    isAnimating = false;
+                    setVisibility(View.INVISIBLE);
+                }
+                @Override
+                public void onAnimationCancel(Animator animation) {
+                    isAnimating = false;
+                }
+                @Override
+                public void onAnimationRepeat(Animator animation) {
+                }
+            };
+        }
+        if(showAnimatorListener == null) {
+            //初始化 展示动画监听事件
+            showAnimatorListener = new Animator.AnimatorListener() {
+                @Override
+                public void onAnimationStart(Animator animation) {
+                    isAnimating = true;
+                    setVisibility(View.VISIBLE);
                 }
 
                 @Override
                 public void onAnimationEnd(Animator animation) {
                     isAnimating = false;
-                    setVisibility(View.INVISIBLE);
                 }
 
                 @Override
@@ -97,14 +119,11 @@ public class AnimatedFloatingButton extends FloatingActionButton {
      */
     public void showAnimation(){
         if(animator != null && getVisibility() != View.VISIBLE) {
-            setVisibility(View.VISIBLE);
-            animator.scaleX(1.0f)
-                    .scaleY(1.0f)
-                    .alpha(1.0f)
-                    .setListener(null);
-        } else {
-                LogUtils.e("None：animator != null && getVisibility() != View.VISIBLE ");
-            }
+//            setVisibility(View.VISIBLE);
+                showScale();
+//                 showTranslation();
+                animator.setListener(showAnimatorListener);
+        }
     }
 
     /**
@@ -112,17 +131,51 @@ public class AnimatedFloatingButton extends FloatingActionButton {
      * 默认动画是 缩放动画 缩小
      */
     public void hideAnimation(){
-        if(animator != null && animatorListener!= null && getVisibility()== View.VISIBLE) {
+        if(animator != null && hideAnimatorListener!= null && getVisibility()== View.VISIBLE) {
             if(! isAnimating) {
-                animator.scaleX(0.0f)
-                        .scaleY(0.0f)
-                        .alpha(0.0f)
-                        .setListener(animatorListener);
+                hideScale();
+//                hideTranslation();
+                animator.setListener(hideAnimatorListener);
             }
-        }else {
-            LogUtils.e("None：animator != null && animatorListener!= null && getVisibility()== View.VISIBLE ");
         }
 
+    }
+
+    private void showScale(){
+        animator.scaleX(1.0f)
+                .scaleY(1.0f)
+                .alpha(1.0f);
+    }
+
+    private void hideScale() {
+        animator.scaleX(0.0f)
+                .scaleY(0.0f)
+                .alpha(0.0f);
+    }
+
+    /**
+     * 向上显示
+     */
+    private void showTranslation(){
+        animator.translationX(0.0f)
+                .translationY( -25.0f  ) //XML布局中marginBottom值
+                .alpha(1.0f);
+
+    }
+
+    /**
+     * 向下隐藏
+     */
+    private void hideTranslation(){
+        animator.translationX(0.0f)
+                .translationY(25.0f + getHeight())
+                .alpha(1.0f);
+    }
+
+    private void getScreenInfo(){
+        DisplayMetrics metrics = App.Companion.getCONTEXT().getResources().getDisplayMetrics();
+        int mScreenHeight = metrics.heightPixels;
+        int mScreenWidth = metrics.widthPixels;
     }
 
 
